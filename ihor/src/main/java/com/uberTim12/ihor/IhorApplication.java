d488307fb7f9efa783c85151591bcdd5324052c9
jovannajdovski <j.najdovski@gmail.com>
2022-11-30 20:37:59 +0100

    init

---
+++ ihor/src/main/java/com/uberTim12/ihor/IhorApplication.java
@@ -0,0 +1,13 @@
+package com.uberTim12.ihor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhorApplication.class, args);
+	}
+
+}
---
+++ ihor/src/test/java/com/uberTim12/ihor/IhorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uberTim12.ihor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
