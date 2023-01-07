c694d21ce9063767f75bb92ed92aba1e6dd383d9
jovannajdovski <j.najdovski@gmail.com>
2023-01-07 16:21:45 +0100

    Png and jpeg format type added for image upload

--- ihor/src/main/java/com/uberTim12/ihor/util/ImageConverter.java
+++ ihor/src/main/java/com/uberTim12/ihor/util/ImageConverter.java
@@ -3,6 +3,9 @@ package com.uberTim12.ihor.util;
 
 import org.springframework.util.Base64Utils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class ImageConverter {
@@ -11,10 +14,17 @@ public class ImageConverter {
         if (imageBytes==null){
             return null;
         }
-        String imageString = null;
-        imageString = Base64Utils.encodeToString(imageBytes);
+        byte[] bytesImageBase64 = Arrays.copyOfRange(imageBytes, 1, imageBytes.length);
 
-        return "data:image/jpeg;base64,"+imageString;
+        String imageString = null;
+        String formatType;
+        imageString = Base64Utils.encodeToString(bytesImageBase64);
+        if ((int)imageBytes[0]==0){
+            formatType = "png";
+        } else {
+            formatType = "jpeg";
+        }
+        return "data:image/"+formatType+";base64,"+imageString;
     }
 
     public static byte[] decodeToImage(String imageString) {
@@ -23,11 +33,25 @@ public class ImageConverter {
             return null;
         }
         String imageBase64;
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         if (imageString.contains(",")){
             imageBase64 = imageString.split(",")[1];
+            String formatType = imageString.split(",")[0].split("/")[1].split(";")[0];
+            System.out.println(formatType);
+            if(formatType.equals("png")){
+                bytes.write(0);
+            } else {
+                bytes.write(1);
+            }
         } else {
             imageBase64 = imageString;
         }
-        return Base64Utils.decodeFromString(imageBase64);
+        try {
+            bytes.write(Base64Utils.decodeFromString(imageBase64));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return bytes.toByteArray();
     }
 }
