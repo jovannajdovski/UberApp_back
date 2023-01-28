52cbb35726e75e6221f37317cc905308835576a7
jovannajdovski <j.najdovski@gmail.com>
2023-01-28 14:23:38 +0100

    Socket for ride chat added

--- ihor/src/main/java/com/uberTim12/ihor/web_socket/RideWebSocketController.java
+++ ihor/src/main/java/com/uberTim12/ihor/web_socket/RideWebSocketController.java
@@ -1,6 +1,7 @@
 package com.uberTim12.ihor.web_socket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uberTim12.ihor.dto.communication.ChatMessageDTO;
 import com.uberTim12.ihor.dto.ride.RideFullDTO;
 import com.uberTim12.ihor.dto.route.LocationDTO;
 import com.uberTim12.ihor.model.ride.Ride;
@@ -42,31 +43,57 @@ public class RideWebSocketController {
     }
 
     @MessageMapping("vehicle/{rideId}/current-location/{token}")
-    public void getVehicleCurrentLocation(@DestinationVariable Integer rideId, @DestinationVariable String token)
-    {
+    public void getVehicleCurrentLocation(@DestinationVariable Integer rideId, @DestinationVariable String token) {
         System.out.println("usao u soket");
-        String userId=jwtUtil.extractId(token);
+        String userId = jwtUtil.extractId(token);
         try {
             Ride ride = rideService.get(rideId);
             if (!userId.equals(ride.getDriver().getId().toString())) {
 
             }
-            if (!passengerInPassengers(userId, new ArrayList<>(ride.getPassengers())))
-            {
+            if (!passengerInPassengers(userId, new ArrayList<>(ride.getPassengers()))) {
 
             }    //nesto
-            Location currentLocation=ride.getDriver().getVehicle().getCurrentLocation();
+            Location currentLocation = ride.getDriver().getVehicle().getCurrentLocation();
             System.out.println("salje ");
-            this.simpMessagingTemplate.convertAndSend("api/socket-publisher/" +"vehicle/current-location/"+rideId , new LocationDTO(currentLocation));
+            this.simpMessagingTemplate.convertAndSend("api/socket-publisher/" + "vehicle/current-location/" + rideId, new LocationDTO(currentLocation));
         } catch (EntityNotFoundException e) {
             //return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ride does not exist!");
         }
     }
+
     private boolean passengerInPassengers(String passengerId, ArrayList<Passenger> passengers) {
         for (Passenger p : passengers)
             if (passengerId.equals(p.getId().toString()))
                 return true;
         return false;
+    }
+
+    @MessageMapping("/send/message/{rideId}/{fromId}")
+    public ChatMessageDTO rideChat(@DestinationVariable Integer rideId, @DestinationVariable Integer fromId, String message) {
+
+        ChatMessageDTO chatMessageDTO = new ChatMessageDTO(message,fromId,rideId);
+        if (rideId != null && rideId != 0 && fromId != null && fromId !=0 ) {
+
+            this.simpMessagingTemplate.convertAndSend("api/socket-publisher/ride-chat/" + rideId,
+                    chatMessageDTO);
+        }
+
+        return chatMessageDTO;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, String> parseMessage(String message) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> retVal;
+
+        try {
+            retVal = mapper.readValue(message, Map.class); // parsiranje JSON stringa
+        } catch (IOException e) {
+            retVal = null;
+        }
+
+        return retVal;
     }
 
 }
